b2fa3de3b32478445f2e66424e6b03cfbdc20b9f
vanderms <vanderms.84@gmail.com>
2022-05-29 17:44:27 -0300

    create products service

---
+++ src/app/products.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, share } from 'rxjs';
+import * as data from '../data/products.json';
+
+export interface IProduct {
+  name: string;
+  reward: boolean;
+  price: number | null;
+  left: number | null;
+  description: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor() { }
+
+  getData() : Observable<IProduct[]> {
+    return of(data).pipe(share());
+  }
+}
