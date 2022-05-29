765c69b34fac4fd1351cd14366925f9264fa3cc6
vanderms <vanderms.84@gmail.com>
2022-05-29 02:28:06 -0300

    create component cover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { CoverComponent } from './cover/cover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule,
